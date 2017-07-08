2739efe5aa7ceb6f0c979c8d72945dd5172be20b
grimcoder <papochka12>
2017-07-08 16:39:49 -0700

    test set attribute

--- index.js
+++ index.js
@@ -2,5 +2,7 @@
  * Created by taraskovtun on 7/8/17.
  */
 exports.disco = function() {
-    alert('disco!');
+    var document = window.document;
+
+    document.body.setAttribute("name", "helloButton");
 }
