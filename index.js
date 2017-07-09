a571a52bb76d8e39cd31979f862cdb56a8a8fe65
grimcoder <papochka12>
2017-07-08 17:10:46 -0700

    test set attribute

--- index.js
+++ index.js
@@ -5,11 +5,33 @@
 
 exports.disco = function() {
 
+    function getRandomIntInclusive(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive
+    }
+
+
     var i = 1;
+    var used = {};
+
     var interval = setInterval(()=>{
         if (i == 10) clearInterval(interval);
 
-        document.body.setAttribute("style", "background-color: #" + (i * 10 - 1) + 'aaaa' );
+        var r = getRandomIntInclusive(10, 99);
+        var g = getRandomIntInclusive(10, 99);
+        var b = getRandomIntInclusive(10, 99);
+
+        var color = '#' + (r+g+b);
+
+        if (used[color]) {
+            i--;
+            return;
+        }
+
+        used[color] = {};
+
+        document.body.setAttribute("style", "background-color: " + color );
 
         i++;
     }, 1000);
